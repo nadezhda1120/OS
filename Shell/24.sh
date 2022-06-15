02b0815cdb2b262c937a648c356fce8c11cc60b8
Nadezhda Pandelieva <55464726+nadezhda1120@users.noreply.github.com>
2022-06-15 17:59:49 +0300

    Create 24.sh

---
+++ Shell/24.sh
@@ -0,0 +1,25 @@
+'''
+#!/bin/bash
+
+if [ $# -gt 2 ] || [ $# -lt 1 ]; then
+        echo "wrong number of arguments";
+        exit 1;
+fi
+
+if [ ! -d $1 ]; then
+        echo "First argumnet should be dir";
+        exit 2;
+fi
+
+if [ $# -eq 2  ]; then
+        if [[  $2 =~ ^[0-9]+$ ]]; then
+                find $1 -type f -printf "%p#%n\n"| awk -F '#' -v num=$2 '{if($2>=num)print $1}'
+        else
+                echo "Second argument should be number";
+                exit 3;
+        fi
+else
+        find -L $1 -type l
+fi
+
+'''
