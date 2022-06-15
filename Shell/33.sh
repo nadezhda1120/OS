15c1e55f172e46595ff2cb3dcf79710dea8077fa
Nadezhda Pandelieva <55464726+nadezhda1120@users.noreply.github.com>
2022-06-15 21:05:18 +0300

    Create 33.sh

---
+++ Shell/33.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+F=$(mktemp)
+
+cat $@ | while read line
+do
+echo "$line"
+#egrep "^\-?[0-9]+$" $line >> $F
+done
+echo "$F"
+max_value=$(cat $F| sort -rn |uniq | head -n 1)
+A=$(cat $F| sort -nr | uniq | egrep "^\-?${max_value$}")
+
+max=-1
+B=0
+while read line; do
+        sum=$(cat $line | sort -n | uniq | tr -d '-' | sed 's/\([0-9]\)/\1 /g'| awk 'BEGIN{total=0}{total+=$1}{print $total}')
+        if [  $sum -gt $max ]; then
+                max=$sum
+                B=$line
+        fi
+done<(cat "$F" | sort -n)
+
+echo "$A \n"
+echo "$B"
+
+
+rm -f -- $F
